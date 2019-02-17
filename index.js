f11dc40379168b487b9e2e45545d98ddb989b463
kkoomin <minha.koo.9@gmail.com>
2019-02-17 22:48:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function printBadges(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`);
+    }
+    return arr;
+}
+
+function tailsNeverFails() {
+    function maybeTrue() {
+        return Math.random() >= 0.5;
+    }
+
+    let numberTails = 0;
+
+    while (maybeTrue()) {
+        numberTails++;
+    }
+
+    return `You got ${numberTails} tails in a row!`;
+
+
+}
